f12aae4803db8cf07b6603cd40fbc91053e1aa4b
shubo7996 <whatsupmofo420@gmail.com>
2019-01-20 09:34:39 +0530

    --Cluster Model--

---
+++ cosmos/clusters/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+import misaka
+from django.urls import reverse
+
+User = get_user_model()
+
+class Cluster(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode= True, unique= True)
+    description = models.CharField(blank= True, default= '' )
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField(User,through= 'ClusterMember')
+
+    def __str__(self):
+        self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka(self.description)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('clusters:single',kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ('name')
+
+class ClusterMember(models.Model):
+    cluster = models.ForeignKey(
+        Cluster,
+        verbose_name = 'subscription'
+    )
+
+    user = models.ForeignKey(
+        User,
+        verbose_name = 'user_cluster'
+    )
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('cluster','user')
+
+
+

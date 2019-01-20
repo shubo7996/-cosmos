084f8e305f76e2a43b4d2ac335dd22b5a4a1c5b4
shubo7996 <whatsupmofo420@gmail.com>
2019-01-20 09:35:45 +0530

    --Post Model--

---
+++ cosmos/posts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+import misaka
+
+from clusters.models import Cluster
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    user = models.ForeignKey(User,verbose_name="posts")
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    cluster = models.ForeignKey(Cluster,verbose_name="posts",null=True,blank=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(save,*args,**kwargs):
+        self.message_html = misaka.html(self..message)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single', kwargs={'username':self.user.username,'pk':self.pk})
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user','message']
